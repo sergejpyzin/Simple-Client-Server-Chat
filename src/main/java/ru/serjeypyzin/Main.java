a723c55f69ddcb39640aeab110c1eafbfafcd027
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-02-02 19:46:56 +0300

    created ServerWindow and SettingServerWindow

--- src/main/java/ru/serjeypyzin/Main.java
+++ src/main/java/ru/serjeypyzin/Main.java
@@ -1,10 +1,13 @@
 package ru.serjeypyzin;
 
 import ru.serjeypyzin.client.ClientWindow;
+import ru.serjeypyzin.server.ServerWindow;
 
 public class Main {
     public static void main(String[] args) {
-        ClientWindow clientWindow = new ClientWindow();
+        ServerWindow serverWindow = new ServerWindow();
+        ClientWindow clientWindow1 = new ClientWindow();
+        ClientWindow clientWindow2 = new ClientWindow();
 
 
 
---
+++ src/main/java/ru/serjeypyzin/server/ServerGUI.java
@@ -0,0 +1,4 @@
+package ru.serjeypyzin.server;
+
+public class ServerGUI {
+}
